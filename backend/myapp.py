2274d0e66ced630876ba95ae6b0510f7c7a1c3c1
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-01-02 19:42:32 -0800

    Update myapp.py

--- backend/myapp.py
+++ backend/myapp.py
@@ -1,14 +1,9 @@
 import os
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 
 from app import app, db
 from app.models import GalleryEntry
 from app.views import *
 
-admin = Admin(name="microblog", template_mode="bootstrap4")
-admin.add_view(ModelView(GalleryEntry, db.session))
-admin.init_app(app)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "XSFDfvbdierhu4erhuf9re0ri409fdbvnhkvdbc djcffd./scd3"
 db.create_all()
 
