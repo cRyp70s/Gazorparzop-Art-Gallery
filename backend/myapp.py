8d8b0f821773571c717a6a75f0e57eedafe096db
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-01-02 19:39:31 -0800

    Update myapp.py

--- backend/myapp.py
+++ backend/myapp.py
@@ -9,7 +9,7 @@ from app.views import *
 admin = Admin(name="microblog", template_mode="bootstrap4")
 admin.add_view(ModelView(GalleryEntry, db.session))
 admin.init_app(app)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY) or "XSFDfvbdierhu4erhuf9re0ri409fdbvnhkvdbc djcffd./scd3"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "XSFDfvbdierhu4erhuf9re0ri409fdbvnhkvdbc djcffd./scd3"
 db.create_all()
 
 if __name__ == "__main__":
